3451891c002fc1730c9500ff95d7050d1033b8c1
nooboftime <tamara_walling@yahoo.com>
2024-08-12 15:08:03 -0500

    app with rapid key for wizard (still testing locally)

--- app.py
+++ app.py
@@ -3,5 +3,32 @@ import requests
 
 app = Flask(__name__)
 
+# RapidAPI key
+RAPIDAPI_KEY = '7387755acemsh5ca89585b8ce843p114431jsn9b4903b4b38a'
 
-will be inserting api's routes in a second
+@app.route('/wizard/fact', methods=['GET'])
+def get_fun_fact():
+    # Constructing the API request
+    api_url = "https://api.fungenerators.com/fact/random"
+    headers = {
+        "X-RapidAPI-Key": RAPIDAPI_KEY,
+        "X-RapidAPI-Host": "api.fungenerators.com"
+    }
+
+    # Fetch the data from the API
+    response = requests.get(api_url, headers=headers)
+    
+    if response.status_code != 200:
+        return jsonify({"error": "Failed to fetch data from the fun fact API"}), 500
+
+    # Parse the response
+    fun_fact_data = response.json()
+
+    # Extracting the information
+    fun_fact = fun_fact_data.get('contents', {}).get('fact', 'No fact available')
+
+    # Return the fun fact as a JSON response
+    return jsonify({"fact": fun_fact})
+
+if __name__ == '__main__':
+    app.run(debug=True)
